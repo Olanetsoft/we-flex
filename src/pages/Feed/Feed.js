12c9f789de1229114614ab4212ccc491d73c3949
olanetsoft <heedris2olubisi@gmail.com>
2020-04-01 19:45:10 +0100

    ROOT URL CHANGED

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -23,7 +23,7 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('http://localhost:8080/auth/status', {
+    fetch('https://we-flex-back-end.herokuapp.com/auth/status', {
       headers: {
         Authorization: 'Bearer ' + this.props.token
       }
@@ -41,7 +41,7 @@ class Feed extends Component {
 
     this.loadPosts();
 
-    const socket = openSocket('http://localhost:8080');
+    const socket = openSocket('https://we-flex-back-end.herokuapp.com');
     socket.on('posts', data => {
       if (data.action === 'create') {
         this.addPost(data.post)
@@ -97,7 +97,7 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page, {
+    fetch('https://we-flex-back-end.herokuapp.com/feed/posts?page=' + page, {
       headers: {
         Authorization: 'Bearer ' + this.props.token
       }
@@ -125,7 +125,7 @@ class Feed extends Component {
 
   statusUpdateHandler = event => {
     event.preventDefault();
-    fetch('http://localhost:8080/auth/status', {
+    fetch('https://we-flex-back-end.herokuapp.com/auth/status', {
       method: 'PATCH',
       headers: {
         Authorization: 'Bearer ' + this.props.token,
@@ -176,10 +176,10 @@ class Feed extends Component {
     formData.append('content', postData.content);
     formData.append('image', postData.image);
 
-    let url = 'http://localhost:8080/feed/post';
+    let url = 'https://we-flex-back-end.herokuapp.com/feed/post';
     let method = 'POST';
     if (this.state.editPost) {
-      url = 'http://localhost:8080/feed/post/' + this.state.editPost._id;
+      url = 'https://we-flex-back-end.herokuapp.com/feed/post/' + this.state.editPost._id;
       method = 'PUT';
     }
 
@@ -230,7 +230,7 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch('http://localhost:8080/feed/post/' + postId, {
+    fetch('https://we-flex-back-end.herokuapp.com/feed/post/' + postId, {
       method: 'DELETE',
       headers: {
         Authorization: 'Bearer ' + this.props.token
--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -14,7 +14,7 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('http://localhost:8080/feed/post/' + postId, {
+    fetch('https://we-flex-back-end.herokuapp.com/feed/post/' + postId, {
       headers : {
         Authorization: 'Bearer ' + this.props.token
       }
@@ -29,7 +29,7 @@ class SinglePost extends Component {
         this.setState({
           title: resData.post.title,
           author: resData.post.creator.name,
-          image: 'http://localhost:8080/' + resData.post.imageUrl,
+          image: 'https://we-flex-back-end.herokuapp.com/' + resData.post.imageUrl,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
           content: resData.post.content
         });
